5be7c1a04e79778bae63be68561ab7c590983bd7
Peter Galonza <p.galonza@evaron.ru>
2024-10-19 23:27:30 +0300

    feature: update menu forwarder

--- build.sh
+++ build.sh
@@ -204,8 +204,10 @@ patch_hekate() {
 
 patch_home_menu() {
     mkdir $BUILD_DIR/games
-    curl -O -L "https://f38d61784492.hosting.myjino.ru/NintendoSwitch/hbmenu_0104444444440000.zip" --output-dir $TMP_DIR
-    $UNZIP_COMMAND $TMP_DIR/hbmenu_*zip -d $BUILD_DIR/games/
+    # curl -O -L "https://f38d61784492.hosting.myjino.ru/NintendoSwitch/hbmenu_0104444444440000.zip" --output-dir $TMP_DIR
+    # $UNZIP_COMMAND $TMP_DIR/hbmenu_*zip -d $BUILD_DIR/games/
+    curl -O -L "https://f38d61784492.hosting.myjino.ru/NintendoSwitch/Homebrew.Menu.nsp" --output-dir $TMP_DIR
+    cp $TMP_DIR/Homebrew.Menu.nsp -d $BUILD_DIR/games/Homebrew.Menu.nsp
 }
 
 patch_homebrew() {
