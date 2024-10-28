9f2d8064c60b8dbe8ac4919a1a1abde5fa944250
Peter Galonza <p.galonza@evaron.ru>
2024-10-29 01:51:14 +0300

    refactor: refactor build

--- build.sh
+++ build.sh
@@ -11,184 +11,187 @@ SRC_ATMOSPHERE="./atmosphere"
 SRC_HOMEBREW="./homebrew"
 
 UNZIP_COMMAND="unzip -o"
+DOWNLOAD_COMMAND="curl --output --fail --output-dir $TMP_DIR --location"
 
 prepare_hekate() {
-    curl -O -L https://github.com/CTCaer/hekate/releases/download/v6.2.1/hekate_ctcaer_6.2.1_Nyx_1.6.3.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/CTCaer/hekate/releases/download/v6.2.1/hekate_ctcaer_6.2.1_Nyx_1.6.3.zip
     $UNZIP_COMMAND $TMP_DIR/hekate_ctcaer_*.zip -d $BUILD_DIR
 }
 
 prepare_atmosphere() {
-    curl -O -L https://github.com/Atmosphere-NX/Atmosphere/releases/download/1.7.1/atmosphere-1.7.1-master-39c201e37+hbl-2.4.4+hbmenu-3.6.0.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/Atmosphere-NX/Atmosphere/releases/download/1.7.1/atmosphere-1.7.1-master-39c201e37+hbl-2.4.4+hbmenu-3.6.0.zip
     $UNZIP_COMMAND $TMP_DIR/atmosphere-*.zip -d $BUILD_DIR
 
-    curl -O -L https://github.com/Atmosphere-NX/Atmosphere/releases/download/1.7.1/fusee.bin --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/Atmosphere-NX/Atmosphere/releases/download/1.7.1/fusee.bin
     cp $TMP_DIR/fusee.bin $BUILD_DIR/bootloader/payloads/fusee.bin
 }
 
 prepare_evaron_atmosphere() {
-    curl -O -L https://github.com/pgalonza/ns-Atmosphere/releases/download/0.1.0/atmosphere-1.7.0-notbranch-dd29b60c3-hbl-2.4.4+hbmenu-3.6.0.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/pgalonza/ns-Atmosphere/releases/download/0.1.0/atmosphere-1.7.0-notbranch-dd29b60c3-hbl-2.4.4+hbmenu-3.6.0.zip
     $UNZIP_COMMAND $TMP_DIR/atmosphere-*.zip -d $BUILD_DIR
 
-    curl -O -L https://github.com/pgalonza/ns-Atmosphere/releases/download/0.1.0/fusee.bin --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/pgalonza/ns-Atmosphere/releases/download/0.1.0/fusee.bin
     cp $TMP_DIR/fusee.bin $TMP_DIR/bootloader/payloads/fusee.bin
 }
 
 prepare_deepsea() {
-    curl -O -L https://github.com/Team-Neptune/DeepSea/releases/download/v4.11.0/deepsea-advanced_v4.11.0.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/Team-Neptune/DeepSea/releases/download/v4.11.0/deepsea-advanced_v4.11.0.zip
     $UNZIP_COMMAND $TMP_DIR/deepsea-advanced_*.zip -d $BUILD_DIR
 }
 
 prepare_kefir() {
-    curl -O -L https://github.com/rashevskyv/kefir/releases/download/746/kefir746.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/rashevskyv/kefir/releases/download/746/kefir746.zip
     $UNZIP_COMMAND $TMP_DIR/kefir_*.zip -d $BUILD_DIR
 }
 
 prepare_gnx() {
-    curl -O -L https://github.com/vncsmnl/GNX/releases/download/18.1.0-02/2024.07.06.GNX.v18.1.0-02.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/vncsmnl/GNX/releases/download/18.1.0-02/2024.07.06.GNX.v18.1.0-02.zip
     $UNZIP_COMMAND $TMP_DIR/*.GNC.*.zip -d $BUILD_DIR
 }
 
 prepare_ultra() {
-    curl -O -L https://github.com/Ultra-NX/Ultra/releases/download/2.2-R4%2B/Ultra.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/Ultra-NX/Ultra/releases/download/2.2-R4%2B/Ultra.zip
     $UNZIP_COMMAND $TMP_DIR/Ultra.zip -d $BUILD_DIR
 }
 
 prepare_next() {
-    curl -O -L https://codeberg.org/vampitech/NeXT/releases/download/3.20/NeXT.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://codeberg.org/vampitech/NeXT/releases/download/3.20/NeXT.zip
     $UNZIP_COMMAND $TMP_DIR/Next.zip -d $BUILD_DIR
 }
 
 prepare_shallowsea() {
-    curl -O -L https://codeberg.org/carcaschoi/Shallowsea/releases/download/v2.23.0/ShallowSea-ams.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://codeberg.org/carcaschoi/Shallowsea/releases/download/v2.23.0/ShallowSea-ams.zip
     $UNZIP_COMMAND $TMP_DIR/ShallowSea-*.zip -d $BUILD_DIR
 }
 
 prepare_4ifir() {
-    curl -O -L https://github.com/rashevskyv/4ifir-checker/raw/9791c36/github/4IFIR.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/rashevskyv/4ifir-checker/raw/9791c36/github/4IFIR.zip
     $UNZIP_COMMAND $TMP_DIR/4IFIR-*.zip -d $BUILD_DIR
 }
 
 prepare_hats() {
-    curl -O -L https://www.mediafire.com/file_premium/p6vnazz54xwl31n/HATS-1.71-5.zip/file --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://www.mediafire.com/file_premium/p6vnazz54xwl31n/HATS-1.71-5.zip/file
     $UNZIP_COMMAND $TMP_DIR/HATS-*.zip -d $BUILD_DIR
 }
 
 prepare_santa_atmo() {
-    curl -O -L https://santa-atmo.ru/nintendo/atmo-1.7.1-150624.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://santa-atmo.ru/nintendo/atmo-1.7.1-150624.zip
     $UNZIP_COMMAND $TMP_DIR/atmo-*.zip -d $BUILD_DIR
 }
 
 prepare_venom() {
-    curl -O -L https://github.com/CatcherITGF/NX-Venom/releases/download/4.9.1/NXVenom.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/CatcherITGF/NX-Venom/releases/download/4.9.1/NXVenom.zip
     $UNZIP_COMMAND $TMP_DIR/NXVenom.zip -d $BUILD_DIR
 }
 
 prepare_switchcraft() {
-    # curl -O -L https://f38d61784492.hosting.myjino.ru/NintendoSwitch/OC_Switchcraft_EOS-1.5.0-atmosphere-1.8.0-prerelease.zip
-    curl -O -L https://github.com/halop/OC-Switchcraft-EOS/releases/download/1.5.0/OC_Switchcraft_EOS.1.5.0.-.atmosphere.1.8.0.prerelease.zip
-    curl -O -L OC_Switchcraft_EOS_*.zip --output-dir $TMP_DIR
+    # $DOWNLOAD_COMMAND https://f38d61784492.hosting.myjino.ru/NintendoSwitch/OC_Switchcraft_EOS-1.5.0-atmosphere-1.8.0-prerelease.zip
+    $DOWNLOAD_COMMAND https://github.com/halop/OC-Switchcraft-EOS/releases/download/1.5.0/OC_Switchcraft_EOS.1.5.0.-.atmosphere.1.8.0.prerelease.zip
+    $DOWNLOAD_COMMAND OC_Switchcraft_EOS_*.zip
 }
 
 prepare_scripts() {
-    curl -O -L https://raw.githubusercontent.com/Atmosphere-NX/Atmosphere/master/utilities/insert_splash_screen.py --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://raw.githubusercontent.com/Atmosphere-NX/Atmosphere/master/utilities/insert_splash_screen.py
     git clone git@github.com:friedkeenan/switch-logo-patcher.git
     git clone git@github.com:zqb-all/convertfb.git
 }
 
 prepare_sigpatches() {
-    # curl -O -L https://sigmapatches.su/sigpatches.zip --output-dir $TMP_DIR
+    # $DOWNLOAD_COMMAND https://sigmapatches.su/sigpatches.zip
     # $UNZIP_COMMAND $TMP_DIR/sigpatches.zip -d $BUILD_DIR
 
-    curl -O -L https://f38d61784492.hosting.myjino.ru/NintendoSwitch/Hekate+AMS-package3-sigpatches-1.8.0P2-cfw-19.0.0_V4.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://f38d61784492.hosting.myjino.ru/NintendoSwitch/Hekate+AMS-package3-sigpatches-1.8.0P2-cfw-19.0.0_V4.zip
     $UNZIP_COMMAND $TMP_DIR/Hekate+AMS-package3-sigpatches-*.zip -d $BUILD_DIR
 }
 
 prepare_overlays() {
-    # curl -O -L https://sigmapatches.su/sys-patch.zip --output-dir $TMP_DIR
-    curl -O -L https://f38d61784492.hosting.myjino.ru/NintendoSwitch/sys-patch-1.5.4.zip --output-dir $TMP_DIR
-    # curl -O -L https://github.com/impeeza/sys-patch/releases/download/v1.5.2/sys-patch-1.5.2-88297f8.zip --output-dir $TMP_DIR
+    # $DOWNLOAD_COMMAND https://sigmapatches.su/sys-patch.zip
+    # $DOWNLOAD_COMMAND https://github.com/impeeza/sys-patch/releases/download/v1.5.2/sys-patch-1.5.2-88297f8.zip
+    $DOWNLOAD_COMMAND https://f38d61784492.hosting.myjino.ru/NintendoSwitch/sys-patch-1.5.4.zip
     $UNZIP_COMMAND $TMP_DIR/sys-patch-*.zip -d $BUILD_DIR
-    # $UNZIP_COMMAND $TMP_DIR/sys-patch-*.zip -d $TMP_DIR
-    # $UNZIP_COMMAND $TMP_DIR/sys-patch.zip -d $BUILD_DIR
     rm $BUILD_DIR/atmosphere/contents/420000000000000B/flags/boot2.flag
 
-    curl -O -L https://github.com/ppkantorski/nx-ovlloader/releases/download/v1.0.9/nx-ovlloader.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/ppkantorski/nx-ovlloader/releases/download/v1.0.9/nx-ovlloader.zip
     $UNZIP_COMMAND $TMP_DIR/nx-ovlloader.zip -d $BUILD_DIR
-    curl -O -L https://github.com/ppkantorski/Ultrahand-Overlay/releases/download/v1.8.0/ovlmenu.ovl --output-dir $TMP_DIR
+
+    $DOWNLOAD_COMMAND https://github.com/ppkantorski/Ultrahand-Overlay/releases/download/v1.8.0/ovlmenu.ovl
     cp -f $TMP_DIR/ovlmenu.ovl $BUILD_DIR/switch/.overlays/ovlmenu.ovl
-    # curl -O -L https://github.com/ppkantorski/EdiZon-Overlay/releases/download/v1.0.9/ovlEdiZon.ovl --output-dir $TMP_DIR
-    curl -O -L https://github.com/proferabg/EdiZon-Overlay/releases/download/v1.0.9/ovlEdiZon.ovl --output-dir $TMP_DIR
+
+    # $DOWNLOAD_COMMAND https://github.com/ppkantorski/EdiZon-Overlay/releases/download/v1.0.9/ovlEdiZon.ovl
+    $DOWNLOAD_COMMAND https://github.com/proferabg/EdiZon-Overlay/releases/download/v1.0.9/ovlEdiZon.ovl
     cp -f $TMP_DIR/ovlEdiZon.ovl $BUILD_DIR/switch/.overlays/ovlEdiZon.ovl
-    curl -O -L https://github.com/ppkantorski/ovl-sysmodules/releases/download/v1.3.1%2B/ovlSysmodules.ovl --output-dir $TMP_DIR
+
+    $DOWNLOAD_COMMAND https://github.com/ppkantorski/ovl-sysmodules/releases/download/v1.3.1%2B/ovlSysmodules.ovl
     cp -f $TMP_DIR/ovlSysmodules.ovl $BUILD_DIR/switch/.overlays/ovlSysmodules.ovl
 
 
-    curl -O -L https://github.com/masagrator/SaltyNX/releases/download/0.9.4/SaltyNX-0.9.4.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/masagrator/SaltyNX/releases/download/0.9.4/SaltyNX-0.9.4.zip
     $UNZIP_COMMAND $TMP_DIR/SaltyNX-*.zip -d $BUILD_DIR
 
-    curl -O -L https://github.com/ppkantorski/Status-Monitor-Overlay/releases/download/v1.1.4%2B/Status-Monitor-Overlay.ovl --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/ppkantorski/Status-Monitor-Overlay/releases/download/v1.1.4%2B/Status-Monitor-Overlay.ovl
     cp -f $TMP_DIR/Status-Monitor-Overlay.ovl $BUILD_DIR/switch/.overlays/Status-Monitor-Overlay.ovl
 }
 
 prepare_sx_gear() {
-    curl -O -L https://f38d61784492.hosting.myjino.ru/NintendoSwitch/SX_Gear_v1.1.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://f38d61784492.hosting.myjino.ru/NintendoSwitch/SX_Gear_v1.1.zip
     $UNZIP_COMMAND $TMP_DIR/SX_Gear_*.zip -d $BUILD_DIR
 }
 
 prepare_payload() {
-    curl -O -L https://f38d61784492.hosting.myjino.ru/NintendoSwitch/Lockpick_RCM_1_9_13.bin --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://f38d61784492.hosting.myjino.ru/NintendoSwitch/Lockpick_RCM_1_9_13.bin
     cp -f $TMP_DIR/Lockpick_RCM_*.bin -d $BUILD_DIR/bootloader/payloads/Lockpick_RCM.bin
 
-    # curl -O -L https://sigmapatches.su/Lockpick_RCM_v1.9.12.zip --output-dir $TMP_DIR
+    # $DOWNLOAD_COMMAND https://sigmapatches.su/Lockpick_RCM_v1.9.12.zip
     # $UNZIP_COMMAND $TMP_DIR/Lockpick_RCM_v1.9.12.zip -d $TMP_DIR/
 
-    curl -O -L https://f38d61784492.hosting.myjino.ru/NintendoSwitch/mod_chip_toolbox.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://f38d61784492.hosting.myjino.ru/NintendoSwitch/mod_chip_toolbox.zip
     $UNZIP_COMMAND $TMP_DIR/mod_chip_toolbox.zip -d $BUILD_DIR/bootloader/payloads/
 
-    curl -O -L https://github.com/suchmememanyskill/TegraExplorer/releases/download/4.2.0/TegraExplorer.bin --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/suchmememanyskill/TegraExplorer/releases/download/4.2.0/TegraExplorer.bin
     cp -f $TMP_DIR/TegraExplorer.bin  -d $BUILD_DIR/bootloader/payloads/TegraExplorer.bin
 }
 
 prepare_homebrew() {
     mkdir $BUILD_DIR/switch/DBI
-    curl -O -L https://github.com/rashevskyv/dbi/releases/download/716ru/DBI.nro --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/rashevskyv/dbi/releases/download/716ru/DBI.nro
     cp -f $TMP_DIR/DBI.nro $BUILD_DIR/switch/DBI/DBI.nro
-    # curl -O -L https://f38d61784492.hosting.myjino.ru/NintendoSwitch/DBI.694.ru.zip --output-dir $TMP_DIR
+
+    # $DOWNLOAD_COMMAND https://f38d61784492.hosting.myjino.ru/NintendoSwitch/DBI.694.ru.zip
     # $UNZIP_COMMAND $TMP_DIR/DBI.694.ru.zip -d $BUILD_DIR/switch/DBI/
 
     mkdir $BUILD_DIR/switch/linkalho
-    curl -O -L https://f38d61784492.hosting.myjino.ru/NintendoSwitch/linkalho-v2.0.1.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://f38d61784492.hosting.myjino.ru/NintendoSwitch/linkalho-v2.0.1.zip
     $UNZIP_COMMAND $TMP_DIR/linkalho-*.zip -d $BUILD_DIR/switch/linkalho
 
     # mkdir $BUILD_DIR/switch/sigpatch-updater
-    # curl -O -L https://bitbucket.org/e1ite007/sigpatch-updater/downloads/sigpatch-updater.nro --output-dir $TMP_DIR
+    # $DOWNLOAD_COMMAND https://bitbucket.org/e1ite007/sigpatch-updater/downloads/sigpatch-updater.nro
     # cp $TMP_DIR/sigpatch-updater.nro $BUILD_DIR/switch/sigpatch-updater/
 
     mkdir $BUILD_DIR/switch/atmo-pack-updater
-    curl -O -L https://github.com/PoloNX/AtmoPackUpdater/releases/download/2.0.1/AtmoPackUpdater.nro --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/PoloNX/AtmoPackUpdater/releases/download/2.0.1/AtmoPackUpdater.nro
     cp -f $TMP_DIR/AtmoPackUpdater.nro $BUILD_DIR/switch/atmo-pack-updater/AtmoPackUpdater.nro
 
-    curl -O -L https://github.com/HamletDuFromage/aio-switch-updater/releases/download/2.23.2/aio-switch-updater.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/HamletDuFromage/aio-switch-updater/releases/download/2.23.2/aio-switch-updater.zip
     $UNZIP_COMMAND $TMP_DIR/aio-switch-updater.zip -d $BUILD_DIR
 
-    # curl -O -L https://github.com/switchbrew/nx-hbloader/releases/download/v2.4.4/hbl.nsp --output-dir $TMP_DIR
+    # $DOWNLOAD_COMMAND https://github.com/switchbrew/nx-hbloader/releases/download/v2.4.4/hbl.nsp
     # cp $TMP_DIR/hbl.nsp $BUILD_DIR/atmosphere/hbl.nsp
 
-    # curl -O -L https://github.com/switchbrew/nx-hbmenu/releases/download/v3.6.0/nx-hbmenu_v3.6.0.zip --output-dir $TMP_DIR
+    # $DOWNLOAD_COMMAND https://github.com/switchbrew/nx-hbmenu/releases/download/v3.6.0/nx-hbmenu_v3.6.0.zip
     # $UNZIP_COMMAND $TMP_DIR/nx-hbmenu_* $BUILD_DIR
 
-    curl -O -L https://github.com/dslatt/nso-icon-tool/releases/download/v0.4.2/nso-icon-tool.nro --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/dslatt/nso-icon-tool/releases/download/v0.4.2/nso-icon-tool.nro
     cp -f $TMP_DIR/nso-icon-tool.nro $BUILD_DIR/switch/atmo-pack-updater/nso-icon-tool.nro
 }
 
 prepare_cheat() {
-    curl -O -L https://github.com/tomvita/Breeze-Beta/releases/download/beta95d/Breeze.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/tomvita/Breeze-Beta/releases/download/beta95d/Breeze.zip
     $UNZIP_COMMAND $TMP_DIR/Breeze.zip -d $BUILD_DIR
 
-    # curl -O -L https://github.com/proferabg/EdiZon-Overlay/releases/download/v1.0.8/ovlEdiZon.ovl --output-dir $TMP_DIR
+    # $DOWNLOAD_COMMAND https://github.com/proferabg/EdiZon-Overlay/releases/download/v1.0.8/ovlEdiZon.ovl
     # cp -f $TMP_DIR/ovlEdiZon.ovl $BUILD_DIR/switch/.overlays/ovlEdiZon.ovl
 
-    curl -O -L https://github.com/tomvita/EdiZon-SE/releases/download/3.8.37/EdiZon.zip --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND https://github.com/tomvita/EdiZon-SE/releases/download/3.8.37/EdiZon.zip
     $UNZIP_COMMAND $TMP_DIR/EdiZon.zip -d $BUILD_DIR
 }
 
@@ -208,9 +211,9 @@ patch_hekate() {
 
 patch_home_menu() {
     mkdir $BUILD_DIR/games
-    curl -O -L "https://f38d61784492.hosting.myjino.ru/NintendoSwitch/hbmenu.nsp" --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND "https://f38d61784492.hosting.myjino.ru/NintendoSwitch/hbmenu.nsp"
     cp $TMP_DIR/hbmenu.nsp -d $BUILD_DIR/games/hbmenu.nsp
-    curl -O -L "https://f38d61784492.hosting.myjino.ru/NintendoSwitch/hbmenu_19.nsp" --output-dir $TMP_DIR
+    $DOWNLOAD_COMMAND "https://f38d61784492.hosting.myjino.ru/NintendoSwitch/hbmenu_19.nsp"
     cp $TMP_DIR/hbmenu_19.nsp -d $BUILD_DIR/games/hbmenu_19.nsp
 }
 
