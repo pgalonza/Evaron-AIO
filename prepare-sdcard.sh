9d7e94688180f49e2db945149c7680daef4df1ff
Peter Galonza <p.galonza@evaron.ru>
2025-05-20 23:29:47 +0300

    feature: update prepare sdcard script

--- prepare-sdcard.sh
+++ prepare-sdcard.sh
@@ -14,7 +14,11 @@ sudo umount /dev/"${DEVICE_BLOCK}"1 || true
 sudo umount /dev/"${DEVICE_BLOCK}"2 || true
 sudo parted --script /dev/"$DEVICE_BLOCK" rm 1 || true
 sudo parted --script /dev/"$DEVICE_BLOCK" rm 2 || true
-sudo parted --script /dev/"$DEVICE_BLOCK" mkpart primary fat32 "${OFFSET}B" "$(python -c "print(($SD_SIZE-$EMUNAND_SIZE_BYTES) - (1+($OFFSET*2)))")B"
-sudo parted --script /dev/"$DEVICE_BLOCK" mkpart primary fat32 "$(python -c "print($SD_SIZE-($EMUNAND_SIZE_BYTES+$OFFSET))")B" "$(python -c "print($SD_SIZE-$OFFSET)")B"
+sudo parted --script /dev/"$DEVICE_BLOCK" unit B mkpart primary fat32 "${OFFSET}B" "$(python -c "print(($SD_SIZE-$EMUNAND_SIZE_BYTES) - (1+($OFFSET*2)))")B"
+sudo parted -s /dev/"$DEVICE_BLOCK" set 1 lba on
+sudo parted --script /dev/"$DEVICE_BLOCK" unit B mkpart primary "$(python -c "print($SD_SIZE-($EMUNAND_SIZE_BYTES+$OFFSET))")B" "$(python -c "print($SD_SIZE-$OFFSET)")B"
+sfdisk --part-type /dev/"$DEVICE_BLOCK" 2 E0
+sudo partprobe /dev/"$DEVICE_BLOCK"
+sync
 
-sudo mkfs.fat -s "$(python -c "print(int((64*1024)/512))")" -F 32 /dev/"${DEVICE_BLOCK}1"
+sudo mkfs.vfat -s "$(python -c "print(int((64*1024)/512))")" -F 32 -R 3318 -n "SWITCH SD" /dev/"${DEVICE_BLOCK}1"
